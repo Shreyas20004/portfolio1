0f6842224cb4505af6ee20e4a6cf0295428e7ede
Shreyas20004 <shreyas2004src@gmail.com>
2023-04-12 23:12:55 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// var a=12 
+// var b =4
+// if(a>b){
+//   console.log(" a bada hai")
+// }
+// else{
+//   console.log("b bada hai")
+// }
+
+// var grade= a 
+// switch (grade){
+//     case "A"
+//     {
+//         console.log('first class')
+//     }
+//     case "B"
+//     {
+//     console.log('second class');}
+// }
+// break;
+// for(let i =0;i<5;i++)
+// console.log(i)
+// function greet() {
+//     console.log("hello dan");
+// }
+// greet();
+// function add( num1,num2){
+//     let sum=num1+num2
+//     return sum;
+// }
+// console.log(add(2,3));
+// function asp(a,b,c){
+//     let asp=a+b-c
+//     return asp;
+// let h1=document.getElementById("docs")
